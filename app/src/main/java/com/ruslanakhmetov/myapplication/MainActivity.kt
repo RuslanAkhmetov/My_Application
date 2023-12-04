9c4424d9074a4ca93dd55904df52f179c7b5e73a
RuslanAkhmetov <akhmrusl@gmail.com>
2023-12-04 20:29:49 +0300

    Initial commit

---
+++ app/src/main/java/com/ruslanakhmetov/myapplication/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.ruslanakhmetov.myapplication
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ruslanakhmetov.myapplication.databinding.ActivityMainBinding
+import com.ruslanakhmetov.myapplication.repository.exceloperation.ConvertToExcel
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var smsData: SMSData
+    lateinit var smsDataMapper: SMSDataMapper
+    lateinit var convertToExcel: ConvertToExcel
+
+    companion object {
+        val sellers = arrayListOf(
+            Seller("METRO.SPB", TransactionSource.ТРАНСПОРТ),
+            Seller("APTECHNOE", TransactionSource.ПРОДУКТЫ),
+            Seller("DIXI-78788D", TransactionSource.ПРОДУКТЫ),
+            Seller("BUSHE", TransactionSource.РАЗВЛЕЧЕНИЯ),
+            Seller("bilet.nspk", TransactionSource.ТРАНСПОРТ)
+        )
+
+        val bankCards = arrayListOf(
+            BankCard("Tinkoff", "*0345", 0.0)
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        smsData = SMSData(applicationContext)
+        smsDataMapper = SMSDataMapper(sellers, bankCards)
+        convertToExcel = ConvertToExcel(applicationContext)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (checkSelfPermission("android.permission.READ_SMS") != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf("android.permission.READ_SMS"), 2)
+        } else {
+
+            val smsList = smsData.readSMSAfterDate(1700636623066L)
+                ?.let { smslist ->    //readSMSFromSender("HoroshSvyaz")  readSMSAfterDate(1700234258465)
+                    var budgetEntries = mutableListOf<BudgetEntry>()
+                    for (sms in smslist) {
+                        sms?.let {
+                            if (sms.sender.equals(bankCards[0].bankName)) {
+                                smsDataMapper.convertSMSToBudgetEntry(it)?.let {entry->
+                                    budgetEntries.add(entry)
+                                    Log.i(TAG, "onStart ......`")
+                                    Log.i(TAG, "smsId: ${entry?.smsId}  ")
+                                    Log.i(TAG, "cardPan: ${entry?.cardPan}  ")
+                                    Log.i(TAG, "trSource: ${entry?.transactionSource}  ")
+                                    Log.i(TAG, "date: ${entry?.date.toString()}  ")
+                                    Log.i(TAG, "balance: ${bankCards[0].balance}")
+                                }
+
+
+                            }
+                        }
+                    }
+                    budgetEntries?.let {
+                        convertToExcel.convertBudgetEntryToExcel(dataList=budgetEntries)
+                    }
+                }
+        }
+
+    }
+}
